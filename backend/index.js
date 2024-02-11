4b0992b929b3ece6fda24bbbbb9893bb0fefe547
Cyano Prem <66469412+cyanoprem@users.noreply.github.com>
2024-02-11 18:25:45 +0530

    Complete basic chat functionality

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const {mongoose, Schema, model } = require('mongoose')
 const http = require('http')
 const { Server } = require('socket.io')
 const cors = require('cors')
@@ -21,15 +22,39 @@ const io = new Server(server, {
   }
 })
 
+
+const messageSchema = new Schema({
+  message: String
+})
+
+const messageModel = model('messages', messageSchema)
+
+
+app.get('/', async (req, res) => {
+  const allMessages = await messageModel.find().exec()
+  res.json(allMessages)
+})
+
+
 io.on("connection", (socket) => {
   console.log(`a user is connected ${socket.id}`)
 
-  socket.on('send_message', (data) => {
-    socket.broadcast.emit('receive_message', data)
+  socket.on('send_message', async (data) => {
+    const newMessage = new messageModel({
+      message: data.message
+    })
+    await newMessage.save()
+
+    const allMessages = await messageModel.find().exec()
+    // console.log(allMessages)
+    io.emit('receive_message', allMessages)
   })
 })
 
 
-server.listen(4000, () => console.log('Listening on PORT 4000'))
+server.listen(4000, async () => {
+  await mongoose.connect('mongodb+srv://burztcrew:0NdsKMN4Ib8CG3oc@cluster0.36xw4ev.mongodb.net/chat-app-db?retryWrites=true&w=majority')
+  console.log('Connected to Chat App DB & Listening on PORT 4000')
+})
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,28 +2,83 @@ import { useEffect, useState } from "react"
 import { io } from "socket.io-client"
 const socket = io.connect('http://localhost:4000')
 
+
+
+// const App = () => {
+
+//   const [message, setMessage] = useState('')
+//   const [msgRec, setMsgRec] = useState('')
+
+//   const sendMessage = () => {
+//     console.log('Btn clicked')
+//     socket.emit('send_message', { message: message })
+//   }
+
+//   useEffect(() => {
+//     socket.on('receive_message', (data) => {
+//       setMsgRec(data.message)
+//     })
+//   }, [socket])
+
+
+//   return <>
+//     <input type="text" placeholder="Enter Message" onChange={(e) => setMessage(e.target.value)} />
+//     <button onClick={sendMessage}>Send Message</button>
+//     <h1>Message Received: {msgRec}</h1>
+//   </>
+// }
+
+// export default App
+
+
 const App = () => {
 
-  const [message, setMessage] = useState('')
-  const [msgRec, setMsgRec] = useState('')
+  const [msgs, setMsgs] = useState([])
+  const [newMsg, setNewMsg] = useState('')
 
-  const sendMessage = () => {
-    console.log('Btn clicked')
-    socket.emit('send_message', { message: message })
-  }
+  useEffect(() => {
+    const fetchAllMsgs = async () => {
+      const response = await fetch('http://localhost:4000/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const data = await response.json()
+      setMsgs(data)
+    }
+    fetchAllMsgs()
+  }, [])
 
   useEffect(() => {
     socket.on('receive_message', (data) => {
-      setMsgRec(data.message)
+      setMsgs(data)
+      // console.log(data)
     })
   }, [socket])
 
 
+
+
+
   return <>
-    <input type="text" placeholder="Enter Message" onChange={(e) => setMessage(e.target.value)} />
-    <button onClick={sendMessage}>Send Message</button>
-    <h1>Message Received: {msgRec}</h1>
+    <input type="text" placeholder="Enter Message" onChange={(e) => setNewMsg(e.target.value)} value={newMsg} />
+
+    <button onClick={() => {
+      socket.emit('send_message', { message: newMsg })
+      // setMsgs([...msgs, newMsg])
+      setNewMsg('')
+    }}>
+      Send Message
+    </button>
+
+    <ul>
+      {
+        msgs.map((msg) => <li>{msg.message}</li>)
+      }
+    </ul>
   </>
+
 }
 
 export default App
